7838f3060d50fc534fbe5c2c68aaaf0e970bf10a
William Chen <wchen@macpro0056.medfusion.local>
2017-04-20 09:24:30 -0400

    Added dependencies and some config stuff

---
+++ webpack.config.dev.js
@@ -0,0 +1,55 @@
+
+
+module.exports = {
+    entry: [
+        './src/index.js'
+    ],
+    module: {
+
+        loaders: [
+            {
+                test: /\.scss?$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loaders: ['react-hot-loader', 'babel-loader', 'eslint-loader']
+            },
+            {
+                test: /\.(ttf|eot|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader: 'file-loader?name=fonts/[name].[ext]'
+            },
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: 'file-loader?name=img/[name].[ext]'
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', '.scss']
+    },
+    output: {
+        path: __dirname + '/www',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        proxy: {
+            '/precheck': {
+                target: {
+                    host: "d1-prechk01.ops.medfusion.net",
+                    protocol: 'http:',
+                    port: 8081
+                },
+                pathRewrite: {
+                    '^/precheck': ''
+                }
+            }
+        },
+        hot: true,
+        contentBase: './www',
+        historyApiFallback: true
+    }
+};
